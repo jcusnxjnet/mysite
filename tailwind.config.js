5e4c71431f96c9219fa499c46c0be14fb8210dd5
jcusnxjnet <frantisek@jcusnxj.net>
2024-12-15 22:10:54 +0100

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./views/**/*.{html,js,njk,md}"],
-  darkMode: 'selector',
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
