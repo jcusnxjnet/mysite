448842d80980ae678a4d4bee20c3256e13295498
jcusnxjnet <frantisek@jcusnxj.net>
2025-01-02 13:41:17 +0100

    Initial commit

---
+++ eleventy.config.js
@@ -0,0 +1,33 @@
+import { DateTime } from "luxon";
+
+export default function(eleventyConfig) {
+	
+	// method creates a file/folder copy in the output directory
+	eleventyConfig.addPassthroughCopy("views/assets/css");
+    eleventyConfig.addPassthroughCopy("views/assets/img");
+    eleventyConfig.addPassthroughCopy("views/assets/js");
+
+	// FILTERS
+	eleventyConfig.addFilter("postDate", (dateObj, format = "DDD", locale = "cs-CZ") => {
+		return DateTime.fromJSDate(dateObj).setLocale(locale).toFormat(format);
+	  });
+
+	// EXCERPTS
+	eleventyConfig.setFrontMatterParsingOptions({
+		excerpt: true,
+		// Optional, default is "---"
+		excerpt_separator: "<!-- excerpt -->",
+	});
+
+};
+
+export const config = {
+    dir: {
+      input: "views",  
+      layouts: "_layouts",
+      output: "dist"
+    },
+
+	markdownTemplateEngine: "njk",
+  	htmlTemplateEngine: "njk",
+  };
---
+++ views/index.md
@@ -0,0 +1,9 @@
+---
+layout: base
+---
+{% for item in collections.post %}
+<h1 class="text-blue-600 font-semibold text-lg"><a href="{{ item.url }}">{{ item.data.title }}</a></h1>
+<p class="text-sm">{{ item.data.date | postDate }}</p>
+<p class="my-4">{{ item.data.page.excerpt}}</p>
+{% endfor %}
+
